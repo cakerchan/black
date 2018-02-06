1b945c71bc223176b772b8b7dc2ef31cf47ab975
chan <875884645@qq.com>
2018-02-06 14:40:17 +0800

    first commit

---
+++ lx/6pxsshpojie.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# coding=utf-8
+from pexpect import pxssh
+
+
+def send_command(s, cmd):
+    s.sendline(cmd)
+    # 匹配prompt(提示符)
+    s.prompt()
+    # 将prompt前所有内容打印出
+    print s.before
+
+
+def connect(host, user, password):
+    try:
+        s = pxssh.pxssh()
+        # 利用pxssh类的login()方法进行ssh登录
+        s.login(host, user, password)
+        return s
+    except:
+        print '[-] Error Connecting'
+        exit(0)
+
+
+s = connect('200.246.101.203', 'root', '6666666')
+send_command(s, 'uname -a')
